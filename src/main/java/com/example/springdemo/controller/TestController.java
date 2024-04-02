b3d3a9ee3dd1aed659c5020451f82979e92af164
neocheng <neocheng@blackfish.cn>
2024-04-02 14:23:36 +0800

    测试代码

---
+++ src/main/java/com/example/springdemo/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.example.springdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName TestController
+ * @Description TODO
+ * @Author neocheng
+ * @Date 2021-03-17 00:12
+ * @Version 1.0
+ **/
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public void test() {
+        Map<Object, Object> data = new HashMap();
+        data.put("params1" , 1);
+        data.put("params2" , 2);
+        return ;
+    }
+}
